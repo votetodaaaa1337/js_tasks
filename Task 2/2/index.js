e0129cb571d6be4d590c8555377abe8140bc27c2
votetodaaaa1337 <144309054+votetodaaaa1337@users.noreply.github.com>
2024-02-02 09:14:21 +0900

    Add files via upload

---
+++ Task 2/2/index.js
@@ -0,0 +1,11 @@
+let numberVar = 10;
+let stringVar = "Hello";
+let booleanVar = true;
+
+numberVar = "New value"; // Теперь numberVar это тип данных string
+stringVar = 100; // Теперь stringVar это тип данных number
+booleanVar = null; // Теперь booleanVar это тип данных null
+
+alert(numberVar);
+alert(stringVar);
+alert(booleanVar);
